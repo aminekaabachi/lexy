fc88f8832a3dbfac06b00100a5efdc8eea26094a
Amine Kaabachi <amine.kaabachi@gmail.com>
2022-01-14 14:18:29 +0100

    fixing tests for csv reading

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -4,12 +4,12 @@ import os
 def test_read_glossary_from_csv():
     filepath = os.path.join(os.path.dirname(__file__), 'fixtures', 'dummy.csv')
     dummy = g.from_csv(filepath)
-    assert dummy("aaaa").metadata == {'hello': 'eeee', 'zoona': 'dfdf'}
-    assert dummy("dfdfd").metadata == {'hello': 'dfddf', 'zoona': 'dfdfd'}
+    assert dummy.find("aaaa").metadata == {'hello': 'eeee', 'zoona': 'dfdf'}
+    assert dummy.find("dfdfd").metadata == {'hello': 'dfddf', 'zoona': 'dfdfd'}
 
 def test_read_glossary_with_no_metadata_from_csv():
     filepath = os.path.join(os.path.dirname(__file__), 'fixtures', 'dummy_nometadata.csv')
     dummy = g.from_csv(filepath)
-    assert dummy("aaaa").definition == "dddd"
-    assert dummy("aaaa").metadata == {}
+    assert dummy.find("aaaa").definition == "dddd"
+    assert dummy.find("aaaa").metadata == {}
 
